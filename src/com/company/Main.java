a4d1c63eedfebc664f26c5dc9785217a9853cc54
swoods30311 <swoods30311@gmail.com>
2021-06-14 01:07:59 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int aNumber = 3;
+        if (aNumber >= 0)
+            if (aNumber == 0)
+                System.out.println("first string");
+            else System.out.println("second string");
+        System.out.println("third string");
+
+    }
+}
